7ccf834fe2c5d73e5318986fac305e5caba5ae5c
smin9408 <stmdaks888@naver.com>
2022-02-21 11:52:32 +0900

    사진 크게 보기 화면 추가  - 라이브러리 활용 X

--- app/src/main/java/com/knta/librarypractice/MainActivity.kt
+++ app/src/main/java/com/knta/librarypractice/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.knta.librarypractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpEvents()
+        setUpValues()
+
+    }
+
+    fun setUpEvents() {
+//        버튼 이외의, TextView, ImageView, LinearLayout 등등도 setOnClickListener로 이벤트 처리가 가능함.
+
+        imgProfile.setOnClickListener {
+
+            val myIntent = Intent(this, ViewPhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
+    }
+
+    fun setUpValues() {
+
     }
 }
